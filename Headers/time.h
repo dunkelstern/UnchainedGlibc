cdfd31015c2f804d50c81e76a4e236c3cbeacb90
Johannes Schriewer <j.schriewer@anfe.ma>
2015-12-21 14:00:50 +0100

    Try overriding the features.h

--- Headers/time.h
+++ Headers/time.h
@@ -1,5 +1,7 @@
-#define _GNU_SOURCE
+#include "features.h"
 #include <time.h>
 
+/*
 extern char *strptime (const char *__restrict __s,
                        const char *__restrict __fmt, struct tm *__tp);
+*/
